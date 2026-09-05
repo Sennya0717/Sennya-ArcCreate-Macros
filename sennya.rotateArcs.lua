9cfe23fab4cf8dc055765cea8ffadb4e8eda28f3
Sennya <krkk970717@gmail.com>
2026-09-05 16:52:01 +0800

    fix: resolve issue where arctap coordinates fail to sync

--- sennya.rotateArcs.lua
+++ sennya.rotateArcs.lua
@@ -50,21 +50,38 @@ Macro.new("rotateArcs")
         local lockHead = userInput.result["lockHead"]
         local lockTail = userInput.result["lockTail"]
 
+        local allArctaps = Event.query(EventSelectionConstraint.create().arctap())
 
         local command = Command.create("rotated notes")
-
+        local arcs = {}
+        local traces = {}
+        local newTraces = {}
+        
         for _, arc in ipairs(selected.resultCombined) do
-            command.add(arc.delete())
             local newStartX = ((arc.startX - anchorX)*2 * math.cos(radians) - (arc.startY - anchorY) * math.sin(radians))/2 + anchorX
             local newStartY = (arc.startX - anchorX)*2 * math.sin(radians) + (arc.startY - anchorY) * math.cos(radians) + anchorY
             local newEndX = ((arc.endX - anchorX)*2 * math.cos(radians) - (arc.endY - anchorY) * math.sin(radians))/2 + anchorX
             local newEndY = ((arc.endX - anchorX)*2 * math.sin(radians)) + (arc.endY - anchorY) * math.cos(radians) + anchorY
             
-            command.add(Event.arc(
+            table.insert(arcs, Event.arc(
                 arc.timing, (not lockHead and newStartX or arc.startX), (not lockHead and newStartY or arc.startY),
                 arc.endTiming, (not lockTail and newEndX or arc.endX), (not lockTail and newEndY or arc.endY),
-                arc.isVoid, arc.color, arc.type, arc.timingGroup, arc.sfx, arc.arcResolutionMultiplier).save()
-            )
+                arc.isVoid, arc.color, arc.type, arc.timingGroup, arc.sfx, arc.arcResolutionMultiplier))
+
+            if arc.is("voidarc") then
+                table.insert(traces, arc)
+                table.insert(newTraces, arcs[#arcs])
+            command.add(arcs[#arcs].save())
+            command.add(arc.delete())
+            end
+            for _, arctap in ipairs(allArctaps.arctap) do
+                for traceIndex, trace in ipairs(traces) do
+                    if arctap.arc.instanceEquals(trace) then
+                        command.add(Event.arctap(arctap.timing,newTraces[traceIndex] , arctap.width).save())
+                        command.add(arctap.delete())
+                    end
+                end
+        end
         end
         command.commit()
     end)
