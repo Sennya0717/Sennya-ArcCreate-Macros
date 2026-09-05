71626d168f2d91c2c1221e68adece0fe49f25a03
Sennya <krkk970717@gmail.com>
2026-09-05 16:53:41 +0800

    Revert "refactor: replace isinstance with =="
    This reverts commit 205a29eed727f52f5a81c36639eeae3223a61a56.

--- sennya.removeTrace.lua
+++ sennya.removeTrace.lua
@@ -22,7 +22,7 @@ Macro.new("removeTrace")
         --重寫黑線、天鍵
         for _, arctap in ipairs(allArctaps.arctap) do
             for _, trace in ipairs(traces) do
-                if arctap.arc == trace then
+                if arctap.arc.instanceEquals(trace) then
                     local timing = arctap.timing
                     table.insert(arcs, Event.arc(
                     timing, arctap.arc.positionAt(timing),
